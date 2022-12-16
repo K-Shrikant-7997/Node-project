69e3ebc6b233ebcb86aea938346720585b02a00b
K Shrikant <shrikantk7997@gmail.com>
2022-12-16 17:15:12 +0530

    First commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import {sum} from "./mymath.js";
+import {mul} from "./mymath.js";
+console.log("hello from Node project!!!");
+let output = sum(10, 12);
+console.log("sum of 10 & 20:-", output);
+let multiplication = mul(10,3);
+console.log("multiplication is:-" , multiplication);

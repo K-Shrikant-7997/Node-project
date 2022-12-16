495261c933fc0c87c00405845acf3222a38a8286
K Shrikant <shrikantk7997@gmail.com>
2022-12-16 18:11:11 +0530

    reading-file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import {sum} from "./mymath.js";
-import {mul} from "./mymath.js";
-console.log("hello from Node project!!!");
-let output = sum(10, 12);
-console.log("sum of 10 & 20:-", output);
-let multiplication = mul(10,3);
-console.log("multiplication is:-" , multiplication);
+import { readFile}  from "node:fs/promises";
+
+async function main(){
+    console.log("READ FILE DEMO");
+    let filepath ="C:\\Users\\admin\\Desktop\\webtechnology\\day1\\1.html";
+    let output =await readFile(filepath , {encoding: "utf8"});
+    console.log(output);
+}
+
+main();
